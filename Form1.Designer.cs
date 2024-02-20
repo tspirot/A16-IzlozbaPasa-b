38475ab592f38223cb89d63ffa105bc4a8d8e6c3
Boban Blagojević <boban.blagojevic@tsp.edu.rs>
2024-02-20 12:57:51 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace A16_IzlozbaPasa_b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.comboBoxKategorija = new System.Windows.Forms.ComboBox();
+            this.comboBoxIzlozba = new System.Windows.Forms.ComboBox();
+            this.comboBoxPas = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.buttonIzlaz = new System.Windows.Forms.Button();
+            this.buttonPrijava = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(0, -2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(978, 601);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonIzlaz);
+            this.tabPage1.Controls.Add(this.buttonPrijava);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.comboBoxKategorija);
+            this.tabPage1.Controls.Add(this.comboBoxIzlozba);
+            this.tabPage1.Controls.Add(this.comboBoxPas);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(970, 572);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Unesi prijavu";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxKategorija
+            // 
+            this.comboBoxKategorija.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxKategorija.FormattingEnabled = true;
+            this.comboBoxKategorija.Location = new System.Drawing.Point(131, 88);
+            this.comboBoxKategorija.Name = "comboBoxKategorija";
+            this.comboBoxKategorija.Size = new System.Drawing.Size(238, 24);
+            this.comboBoxKategorija.TabIndex = 5;
+            // 
+            // comboBoxIzlozba
+            // 
+            this.comboBoxIzlozba.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxIzlozba.FormattingEnabled = true;
+            this.comboBoxIzlozba.Location = new System.Drawing.Point(131, 58);
+            this.comboBoxIzlozba.Name = "comboBoxIzlozba";
+            this.comboBoxIzlozba.Size = new System.Drawing.Size(399, 24);
+            this.comboBoxIzlozba.TabIndex = 4;
+            // 
+            // comboBoxPas
+            // 
+            this.comboBoxPas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPas.FormattingEnabled = true;
+            this.comboBoxPas.Location = new System.Drawing.Point(131, 28);
+            this.comboBoxPas.Name = "comboBoxPas";
+            this.comboBoxPas.Size = new System.Drawing.Size(238, 24);
+            this.comboBoxPas.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kategorija";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Izložba";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pas";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(970, 572);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Statistika";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(970, 572);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "O aplikaciji";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(970, 572);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Izlaz";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // buttonIzlaz
+            // 
+            this.buttonIzlaz.BackgroundImage = global::A16_IzlozbaPasa_b.Properties.Resources.zatvori;
+            this.buttonIzlaz.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonIzlaz.Location = new System.Drawing.Point(232, 177);
+            this.buttonIzlaz.Name = "buttonIzlaz";
+            this.buttonIzlaz.Size = new System.Drawing.Size(75, 71);
+            this.buttonIzlaz.TabIndex = 8;
+            this.buttonIzlaz.UseVisualStyleBackColor = true;
+            // 
+            // buttonPrijava
+            // 
+            this.buttonPrijava.BackgroundImage = global::A16_IzlozbaPasa_b.Properties.Resources.dodaj;
+            this.buttonPrijava.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonPrijava.Location = new System.Drawing.Point(79, 177);
+            this.buttonPrijava.Name = "buttonPrijava";
+            this.buttonPrijava.Size = new System.Drawing.Size(75, 71);
+            this.buttonPrijava.TabIndex = 7;
+            this.buttonPrijava.UseVisualStyleBackColor = true;
+            this.buttonPrijava.Click += new System.EventHandler(this.buttonPrijava_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::A16_IzlozbaPasa_b.Properties.Resources.pasmacka;
+            this.pictureBox1.Location = new System.Drawing.Point(578, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(362, 213);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 603);
+            this.Controls.Add(this.tabControl1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1000, 650);
+            this.Name = "Form1";
+            this.Text = "Prijava na izložbu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBoxKategorija;
+        private System.Windows.Forms.ComboBox comboBoxIzlozba;
+        private System.Windows.Forms.ComboBox comboBoxPas;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonPrijava;
+        private System.Windows.Forms.Button buttonIzlaz;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A16_IzlozbaPasa_b
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection konekcija;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                konekcija = new SqlConnection
+                    (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\A16.mdf;Integrated Security=True");
+                //citanje iz baze
+                OsveziPse();
+                OsveziIzlozbe();
+                OsveziKategorije();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Greska u konekciji");
+            }
+        }
+        // popunjavanje comboboxa pasa
+        private void OsveziPse()
+        {
+            try
+            {
+                SqlCommand komanda = new SqlCommand("SELECT PasID, " +
+                    "CONCAT(PasID,' - ',Ime) AS ImePsa FROM Pas", konekcija);
+                SqlDataAdapter adapter = new SqlDataAdapter(komanda);
+                DataTable tabela = new DataTable();
+                adapter.Fill(tabela);
+                comboBoxPas.Items.Clear();
+                comboBoxPas.DataSource = tabela;
+                comboBoxPas.DisplayMember = "ImePsa";
+                comboBoxPas.ValueMember = "PasID";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greska u citanju pasa:" + ex.Message);
+            }
+        }
+        // popunjavanje comboboxa za Izlozbe
+        private void OsveziIzlozbe()
+        {
+            try
+            {
+                SqlCommand komanda = new SqlCommand("SELECT IzlozbaID, " +
+                    "CONCAT(IzlozbaID,' - ',Mesto,' - ',Datum)" +
+                    "AS NazivIzlozbe FROM Izlozba", konekcija);
+                SqlDataAdapter adapter = new SqlDataAdapter(komanda);
+                DataTable tabela = new DataTable();
+                adapter.Fill(tabela);
+                comboBoxIzlozba.Items.Clear();
+                comboBoxIzlozba.DataSource = tabela;
+                comboBoxIzlozba.DisplayMember = "NazivIzlozbe";
+                comboBoxIzlozba.ValueMember = "IzlozbaID";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greska u citanju izlozbe:" + ex.Message);
+            }
+        }
+        // popunjavanje comboboxa kategorija
+        private void OsveziKategorije()
+        {
+            try
+            {
+                SqlCommand komanda = new SqlCommand("SELECT KategorijaID, " +
+                    "CONCAT(KategorijaID,' - ',Naziv) AS ImeKategorije FROM Kategorija", 
+                    konekcija);
+                SqlDataAdapter adapter = new SqlDataAdapter(komanda);
+                DataTable tabela = new DataTable();
+                adapter.Fill(tabela);
+                comboBoxKategorija.Items.Clear();
+                comboBoxKategorija.DataSource = tabela;
+                comboBoxKategorija.DisplayMember = "ImeKategorije";
+                comboBoxKategorija.ValueMember = "KategorijaID";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Greska u citanju kategorija:" + ex.Message);
+            }
+        }
+
+        private void buttonPrijava_Click(object sender, EventArgs e)
+        {
+            if(comboBoxPas.SelectedIndex == -1 
+                || comboBoxIzlozba.SelectedIndex == -1 
+                || comboBoxKategorija.SelectedIndex == -1)
+            {
+                MessageBox.Show("Morate izabrati psa, izlozbu i kategoriju");
+                return;
+            }
+            string provera = "select count(*) from Rezultat " +
+                "where PasID=@Pas " +
+                "and IzlozbaID=@Izlozba "+
+                "and KategorijaID=@Kategorija";
+            SqlCommand komandaProvera = new 
+                SqlCommand(provera, konekcija);
+            komandaProvera.Parameters.AddWithValue
+                ("@Pas", comboBoxPas.SelectedValue);
+            komandaProvera.Parameters.AddWithValue
+                ("@Izlozba", comboBoxIzlozba.SelectedValue);
+            komandaProvera.Parameters.AddWithValue
+                ("@Kategorija", comboBoxKategorija.SelectedValue);
+            konekcija.Open();
+            int broj = (int)komandaProvera.ExecuteScalar();
+            if(broj > 0)
+            {
+                MessageBox.Show("Pas je vec prijavljen za izlozbu u toj kategoriji");
+                konekcija.Close();
+                return;
+            }
+            // prijava psa na izlozbu
+        }
+    }
+}
